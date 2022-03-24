c38625b0789634e0af87cc3d941428ea9786866f
Mianca05 <macastillo18@espe.edu.ec>
2022-03-24 12:39:58 -0500

    Correccion Ejercicio8.py y agrego Ejercicio10.py

---
+++ Ejercicios_Listas/Ejercicio10.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 23:09:49 2022
+
+@author: MIANCAS
+"""
+# =============================================================================
+# EJERCICIO 10: Crear un programa que lea por teclado una tabla de 10 números enteros 
+# y desplace N posiciones en el arreglo (N es digitado por el usuario). 
+# =============================================================================
+
+#variables
+lista = []
+lista_backup = []
+#entrada 
+for i in range (10):
+    lista.append(int(input(f"{i+1}. Ingrese un numero: ")))
+
+numero_posicion = int(input("Ingrese una posición a desplazar entre 1 - 9: "))
+while numero_posicion < 0 or numero_posicion >9 :
+    numero_posicion = int(input("Ingrese una posición a desplazar entre 1 - 9: "))
+
+
+# dezplazar n posiciones
+j = 0
+for i in range(10):
+    if i + numero_posicion < 10:
+        nueva_posicion = i + numero_posicion
+        lista_backup.insert(nueva_posicion, lista[j])
+    if i + numero_posicion > 9:
+        nueva_posicion = abs((i + numero_posicion) - len(lista) )
+        lista_backup.insert(nueva_posicion, lista[j])
+        
+    j += 1
+
+#print(len(lista_backup))   
+print(lista_backup)
--- Ejercicios_Listas/Ejercicio8.py
+++ Ejercicios_Listas/Ejercicio8.py
@@ -24,17 +24,19 @@ numero_usuario = int(input("Ingrese un nuevo numero: "))
 posicion_usuario = int(input("Ingrese una posicion para el nuevo numero: "))
 
 #crear backup 
-contador_posicion = posicion_usuario
-j = 0
-longitud_backup = len(lista_numeros)-posicion_usuario
-while j < (longitud_backup):
-    backup_listaNumeros.append(lista_numeros[contador_posicion])
-    contador_posicion +=1
-    j+=1
-
-#mostrar backup
-for i in range(longitud_backup):
-    print(backup_listaNumeros[i])
+# =============================================================================
+# contador_posicion = posicion_usuario
+# j = 0
+# longitud_backup = len(lista_numeros)-posicion_usuario
+# while j < (longitud_backup):
+#     backup_listaNumeros.append(lista_numeros[contador_posicion])
+#     contador_posicion +=1
+#     j+=1
+# 
+# #mostrar backup
+# for i in range(longitud_backup):
+#     print(backup_listaNumeros[i])
+# =============================================================================
 
 #insertar elemento nuevo en la lista (cada vez que se ingresa un nuevo elemento la lista aumenta de tamanio)
 lista_numeros.insert(posicion_usuario, numero_usuario)
