a6922c69c146af5bea3acf3df448bdf926c6ab35
Mianca05 <miancas05sd@gmail.com>
2022-03-26 23:54:53 -0500

    Uso de modulos
    Se revisa como importar modulos preconstruidos, de terceros(pip) y propios

---
+++ modmath.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 23:25:41 2022
+
+@author: MIANCAS
+"""
+
+#SE crea un modulo con dos operaciones basicas de suma y resta
+
+def sumar(n1,n2):
+    print(n1+n2)
+
+def restar(n1,n2):
+    print(n1-n2)
---
+++ modulos.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 22:40:07 2022
+
+@author: MIANCAS
+"""
+
+# =============================================================================
+# #MODULOS: Permiten crear aplicaciones reales. La razón de su uso es evitar tareas repetitivas.
+#Existen 3 tipos de módulos
+#1. Módulo propio
+#2. Módulo de Internet o de Terceros (Pip)
+#3. Módulos de las propias bibliotecas de python
+# =============================================================================
+
+#FORMA 1 para importar
+#import datetime
+
+#devuelve la fecha actual
+#print(datetime.date.today())
+
+#devuelve la cantidad de horas y minutos
+#print(datetime.timedelta(minutes=70))
+
+
+
+#FORMA 2 para importar
+# se lee: desde la biblioteca datetime importa timedelta
+from datetime import timedelta
+
+#devuelve la cantidad de horas y minutos
+print(timedelta(minutes=110))
+
+
+# IMPORTO MODULO PROPIO modmath
+from modmath import sumar, restar
+sumar(23, 5)
+restar(23, 5)
