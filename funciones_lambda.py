db52ae453611cad5e3ee6dbde4a412f10e1771cd
Mianca05 <miancas05sd@gmail.com>
2022-03-26 23:53:36 -0500

    Uso de funcion lambda

---
+++ funciones_lambda.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 21:23:56 2022
+
+@author: MIANCAS
+"""
+
+# =============================================================================
+# FUNCIONES LAMBDA
+# Son funciones anónimas que toman un numero de argumentos pero que estas solo
+# pueden ser escritas utilizando  una sola expresion.
+# =============================================================================
+
+suma = lambda numero1, numero2: numero1 + numero2
+print(suma(5, 18)) 
