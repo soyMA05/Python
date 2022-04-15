fa38935cf708ea93bdc1b6c218a28be7dbcb3bb8
Mianca05 <miancas05sd@gmail.com>
2022-04-14 19:25:19 -0500

    Update encapsulamiento_y_ocultamiento.py
    Se usa el Encapsulamiento en la clase Carrera.
    Se usa Ocultamiento en la clase Materia usando decorador @property que hace referencia al getter y el decorador @atributo.setter hace referencia al setter de java

--- POO/encapsulamiento_y_ocultamiento.py
+++ POO/encapsulamiento_y_ocultamiento.py
@@ -12,17 +12,34 @@ class Carrera:
         #self.materias = [] #lista
 
     def agregarMaterias(self, codigo, materia):
-        self.materias[codigo]= materia
+        self.materias[codigo]= materia #aqui está el ENCAPSULAMIENTO porque agrego al diccionario materias de la clase Materia... 
+        #que posteriormente su estructura no podrá ser modificado ya que se supone que tiene todos los atributos y métodos necesarios
         
 class Materia:
-    def __init__(self, nombre, profesor):
+    def __init__(self, nombre, profesor, fecha):
         self.nombreMateria = nombre
         self.nombreProfesor = profesor
+        #OCULTAMIENTO (Forma 1) de aqui para abajo
+        #nuevo atributo que no puede ser anterior a 2006
+        self.fechaInicioDictadoMateria = fecha # primero pasa por una validacion (los metodos de abajo) y luego se asigna
+        
+    @property # PROPERTY enmascara o envuelve un atributo dentro de una propiedad
+    def fechaInicioDictadoMateria(self):
+        #print("prueba que primero pasa por aqui para luego asignar")
+        return self._fechaInicioDictadoMateria #atributo privado, es decir que esta protegido o que esta oculto
+    
+    #SETTER permite modificar o hacer la validación para luego confirmar la asignacion
+    @fechaInicioDictadoMateria.setter
+    def fechaInicioDictadoMateria(self, fecha):
+        if fecha < 2006:
+            self._fechaInicioDictadoMateria = 2006
+        else:
+            self._fechaInicioDictadoMateria = fecha
         
 ingenieria = Carrera("Ingenieria")
-algebra = Materia("Algebra", "Miguel Castillo")
-fisica = Materia("Fisica", "Blanca Vargas")
-quimica = Materia("Quimica", "Esperanza Enriquez")
+algebra = Materia("Algebra", "Miguel Castillo", 2005)
+fisica = Materia("Fisica", "Blanca Vargas",2022)
+quimica = Materia("Quimica", "Esperanza Enriquez",2022)
 print(algebra.nombreProfesor)
 
 """
@@ -33,5 +50,9 @@ print(len(ingenieria.materias))
 print(ingenieria.materias)
 print(algebra.nombreMateria)
 """
+#ENCAPUSALAMIENTO
 ingenieria.agregarMaterias(123, algebra)
 print(ingenieria.materias)
+
+# OCULTAMIENTO
+print(algebra.fechaInicioDictadoMateria)
