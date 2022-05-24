f404955b69728928354665ec2846dc2bfd2268d8
soyMA05 <miancas05sd@gmail.com>
2022-05-23 19:50:51 -0500

    Clase con metodos operaciones y funciones con *args

--- POO/claseOperaciones.py
+++ POO/claseOperaciones.py
@@ -30,9 +30,30 @@ class Operaciones(object):#segun yo es opcional poner object ya que da lo mismo
             return round(resultado,2)
     
 
-o = Operaciones()
+o = Operaciones()#instancia de clase
 print(o.sumar(5,6))
+
 print(o.restar(5, 2))
+
 print(o.multiplicar(5, 3))
+
 print(o.dividir(5, 3))
+
+##funcion suma pasando varios numeros de parametros
+def sumaSinLimites(*args):
+    return sum(args)
+
+print(sumaSinLimites(1,2,3,4))
+print(sum([1,1,1,1,2]))
+
+#sumando cuadrados de varios numeros
+def sumaCuadrados(*numeros):
+    total=0
+    for n in numeros:
+        total += n**2
+    return total
+
+print('Total suma cuadrados: '+ str(sumaCuadrados(1,2,3,4,5)))
+
+
 
