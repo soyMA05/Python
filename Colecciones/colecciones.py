81059b31d07b7720a84e6530746f3c2c9acbae90
soyMA05 <miancas05sd@gmail.com>
2022-05-25 00:03:30 -0500

    Uso de libreria collections para crear diccionarios dinamicos

--- Colecciones/colecciones.py
+++ Colecciones/colecciones.py
@@ -10,7 +10,7 @@ Created on Tue May 17 23:13:10 2022
 """
 1. Counter: es un contenedor que almacena los elementos como claves de diccionario y sus
 cuentas(numero de veces que se repite el elemento) como valores de diccionario.
-
+"""
 from collections import Counter
 a = "aabccddd"
 my_counter = Counter(a)
@@ -20,25 +20,25 @@ print(my_counter.most_common(2))#elementos de diccionario que mas se repite
 print(my_counter.most_common(1)[0])#elemento la lista del diccionario que mas se repite
 #llevar a una lista los elementos(claves) del diccionario
 print(list(my_counter.elements()))
-"""
+
 
 """
 2. Namedtuple (tupla nombrada) son un tipo de objeto ligero y facil de crear similar a una estructura, permite 
 crear subclases de tuplas donde el primer campo es la clase que se instancia y el segundo campo son los valores. 
-
+"""
 from collections import namedtuple
 
 Point = namedtuple('Point', ['x','y'])
 punto = Point(4, 5)
 print(punto) #imprime la clase Point, mostrando coordenadas asi x=4  y y=5.
 print(punto.x, punto.y) #imprime unicamente los valores de las variables x y y.
-"""
+
 
 """
 3. OrderedDict (dictado ordenado): es como un diccionario normal que recuerda el orden en que se insertaron los 
 elementos. 
 Nota: ya no es muy usado porque desde la clase diccionario integrada en python >=3.7 se puede hacer eso.
-
+"""
 from collections import OrderedDict
 diccionario_ordenado = OrderedDict()
 clave=" "#nombre
@@ -50,7 +50,7 @@ for i in range(3):
 
 print(diccionario_ordenado.items())#muestra el orden en que ingrese los elementos a diferencia de muestre por orden A-Z
 
-"""
+
 
 """
 4. DefaultDict (diccionario predeterminado): tendrá un valor predeterminado si la clave aun no se ha configurado.
