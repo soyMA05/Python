93a69a97e835c40ad4d9262fc467c3d93719619f
soyMA05 <miancas05sd@gmail.com>
2022-06-16 22:29:33 -0500

    Conociendo como se crean las clases en python

---
+++ POO/PrimerosPasos_clases.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 22:03:54 2022
+
+@author: miancastillo
+"""
+
+""" PARTE 1: MANERAS DE CREAR CLASES """
+
+""" 1. Con variables """
+print(type(5)) #este es una clase de tipo int
+Item = "Telefono"#variable Item de tipo string
+Item_precio = 200 #variable de tipo int
+Item_cantidad = 2
+print(type(Item))#se visualiza la clase de tipo str o string
+print(type(Item_precio))#se visualiza la clase de tipo int
+print(type(Item_cantidad))#se visualiza la clase de tipo int
+
+
+""" 2. Usando la palabra clave class """
+class Item1:
+    pass #permite crear clases minimas. Tambien le indica al programa que ignore cierta condicion y continue con el resto.
+    
+item1 = Item1() #instancia de clase Item1
+
+""" Atributos de la clase Item1 """
+item1.name = "Telefono"
+item1.precio = 200
+item1.cantidad = 2
+
+print(item1.name)
