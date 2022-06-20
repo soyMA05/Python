a22236f8766cab16f4e6786bde9d977caa514fdd
soyMA05 <miancas05sd@gmail.com>
2022-06-20 18:54:16 -0500

    Validaciones de entrada basicas

--- POO/2_Constructor-def__init__.py
+++ POO/2_Constructor-def__init__.py
@@ -23,7 +23,7 @@ item = Item()
 class Item1:
     
     #constructor
-    def __init__(self, nombre, precio, cantidad):
+    def __init__(self, nombre:str, precio:float, cantidad=0): 
         self.nombre = nombre
         self.precio = precio
         self.cantidad = cantidad
@@ -33,5 +33,15 @@ class Item1:
         return self.cantidad * self.precio
 
 """ Instancia de clase Item1 """
-item = Item1("Laptop", 780, 2)
+item = Item1("Laptop", 5, "1")
 print(item.calcular_total_precio())# invocacion de metodo desde el objeto creado a partir de la instancia de clase Item1
+
+
+
+""" NOTAS:
+    1. Para validar un campo de tipo entero no es necesario poner cantidad: int, porque python reconocer por defecto los 
+        numeros enteros. Tambien, se puede usar una valor predeterminado, ejemplo: cantidad=0. Este valor se usara cuando
+        no se pase algun valor y si se pasa un valor tomara el primero y no el que esta por defecto.
+    
+    
+"""
