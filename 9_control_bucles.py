8ec1eb89c1cc8f7531db2116f4229f4317e172ab
soyMA05 <miancas05sd@gmail.com>
2023-06-01 14:07:55 -0500

    sentencias de control de bucles

--- 9_control_bucles.py
+++ 9_control_bucles.py
@@ -17,6 +17,22 @@ pasar = no hace nada, actua como un marcador de posicion.
 while True:
     name = str(input('What is your name?'))
     if name != "":
+        print('Hi %s' %name)
         break
     else:
-        print('Hi %s' %name)
+        print('Ingrese correctamente su nombre')
+
+
+#continue
+"""en este ejercicio solo quiero obtener los numeros y no caracteres especiales"""
+phone_number = "02-2715-565"
+for i in phone_number:
+    if i == "-":
+        continue #aqui puedo hacer un punto de control para tomar un valor especifico
+    print(i, end="")
+
+#pass
+for i in range(1,21):
+    if i == 13:
+        pass #aqui puedo evitar que se considere un valor para que continue el programa
+    print(i, end=" ")
