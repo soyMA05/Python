3bcf2163ae61df6e4d38a92a2625a007491c2093
soyMA05 <miancas05sd@gmail.com>
2023-06-21 10:47:51 -0500

    bases de multiherencia

---
+++ POO/bc_21_multiherencia.py
@@ -0,0 +1,30 @@
+#multiherencia: es cuando una clase derivada (hijo) hereda otra clase derivada (hijo)
+
+#clase padre
+class Organism:
+    alive=True
+
+
+#clase hija
+class Animal(Organism):
+    def eat(self):
+        print("This animal is eating")
+
+#clase hija de hija
+class Dog(Animal):
+    def bark(self):
+        print("This dog is barking")
+
+
+#instanciar clase/crear objeto
+dog = Dog()
+
+#imprimir atributo de clase padre Organismo
+print(dog.alive)
+
+#imprimir metodo de clase padre Animal
+dog.eat()
+
+#imprimir metodo de clase
+dog.bark()
+
